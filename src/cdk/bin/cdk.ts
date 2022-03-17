d7994c8c9f6e1752febe4caa95206c7133f3bd28
hendryanw <hendry.anwar@live.com>
2022-03-17 15:02:21 +0700

    Implement appstream on CDK

--- src/cdk/bin/cdk.ts
+++ src/cdk/bin/cdk.ts
@@ -1,21 +1,13 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { AppStreamLtiStack } from '../lib/appstream-lti-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new AppStreamLtiStack(app, 'appstream-lti-stack', {
+  fleetName: 'appstream-lti-fleet',
+  fleetDisplayName: 'appstream-lti-fleet',
+  fleetImageName: 'Amazon-AppStream2-Sample-Image-02-04-2019',
+  stackName: 'appstream-lti-stack',
+  createAppStreamIamServiceRole: false
 });
---
+++ src/cdk/lib/appstream-lti-stack.ts
@@ -0,0 +1,84 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as appstream from 'aws-cdk-lib/aws-appstream';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+export interface AppStreamLtiStackProps extends StackProps {
+  fleetName: string;
+  fleetDisplayName: string;
+  fleetImageName: string;
+  stackName: string;
+  createAppStreamIamServiceRole: boolean;
+}
+
+export class AppStreamLtiStack extends Stack {
+  constructor(scope: Construct, id: string, props: AppStreamLtiStackProps) {
+    super(scope, id, props);
+    
+    // VPC
+    const vpc = new ec2.Vpc(this, 'vpc', {
+      maxAzs: 2
+    });
+
+    const privateSubnets = vpc.selectSubnets({
+      subnetType: ec2.SubnetType.PRIVATE_WITH_NAT
+    }).subnetIds;
+
+    // AppStream
+    const fleet = new appstream.CfnFleet(this, 'appstream-fleet', {
+      name: props.fleetName,
+      displayName: props.fleetDisplayName,
+      imageName: props.fleetImageName,
+      instanceType: 'stream.standard.medium',
+      fleetType: 'ON_DEMAND',
+      computeCapacity: {
+        desiredInstances: 1
+      },
+      vpcConfig: {
+          subnetIds: privateSubnets,
+          securityGroupIds: [ vpc.vpcDefaultSecurityGroup ]
+      },
+      maxUserDurationInSeconds: 57600,
+      disconnectTimeoutInSeconds: 900,
+      enableDefaultInternetAccess: false,
+      streamView: 'DESKTOP',
+    });
+    fleet.addOverride('CreationPolicy', {
+      'StartFleet': 'true'
+    });
+
+    const homeFolderBucket = new s3.Bucket(this, 'home-folder-bucket', {
+      versioned: false
+    });
+
+    const stack = new appstream.CfnStack(this, 'appstream-stack', {
+      name: props.stackName,
+      storageConnectors: [{
+        connectorType: 'HOMEFOLDERS',
+        resourceIdentifier: homeFolderBucket.bucketArn
+      }]
+    });
+
+    if (props.createAppStreamIamServiceRole) {
+      const appStreamRole = new iam.Role(this, 'appstream-service-role', {
+        managedPolicies: [ iam.ManagedPolicy.fromManagedPolicyArn(this, 'AmazonAppStreamServiceAccess', 'arn:aws:iam::aws:policy/service-role/AmazonAppStreamServiceAccess') ],
+        roleName: 'AmazonAppStreamServiceAccess',
+        path: '/service-role/',
+        assumedBy: new iam.ServicePrincipal('appstream.amazonaws.com')
+      });
+      stack.node.addDependency(appStreamRole);
+    }
+
+    const stackFleetAssociation = new appstream.CfnStackFleetAssociation(this, 'FullDesktopStackFleetAssociation', {
+      fleetName: fleet.name,
+      stackName: stack.name!
+    });
+    stackFleetAssociation.addDependsOn(fleet);
+    stackFleetAssociation.addDependsOn(stack);
+
+    // LTI API Gateway and Lambda Function
+    
+  }
+}
